e51187e82f489d21ebe07761f3278f9c0bcd01d7
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-30 20:25:02 +0100

    chore: setup project folder and file structure

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ class App extends React.Component {
   render() {
     return(
       <div data-test="component-app">
-        <h1>App</h1>
+        <h1>App</h1> 
       </div>
     );
   }
 }
-
-export default App;
