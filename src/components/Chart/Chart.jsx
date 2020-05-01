2677b10ff847d0008c12257210b40db9ecdd8364
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-01 19:10:31 +0100

    feat(test): test-drive create chart functional component

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chart = () => {
+  return (
+    <div data-test="component-chart">Chart</div>
+  )
+}
+
+export default Chart
---
+++ src/components/Chart/Chart.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import { findByTestAttr } from '../../test/testUtils';
+
+import Chart from './Chart';
+
+const setup = (props={}) => {
+  const wrapper = shallow(<Chart {...props} />)
+  return wrapper;
+}
+
+it('renders chart component without error', () =>{
+  const wrapper = setup();
+  const chartComponent = findByTestAttr(wrapper, 'component-chart');
+  expect(chartComponent.length).toBe(1);
+});
