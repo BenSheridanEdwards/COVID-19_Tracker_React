b759d3455ed99474f8fc857570b317a1acf32905
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-04 22:31:10 +0100

    fix: typos in function component name and export default name

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { NativeSelect, FormControl } from '@material-ui/core'
 
-const Cards = () => {
+const CountryPicker = () => {
   return (
-    <h1 data-test="component-country-picker">CountryPicker</h1>
+    <div data-test="component-country-picker">CountryPicker</div>
   )
 }
 
-export default Cards
+export default CountryPicker
