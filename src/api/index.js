97b8b4cb0d16c9861f31cdd7897a6ab6e5ac4c11
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-06 01:38:32 +0100

    feat: fetch country data from COVID-19 API

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changebleUrl = url;
+
+  if(country) {
+    changebleUrl = `${url}/countries/${country}`
+  }
+
+
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changebleUrl);
 
     return { confirmed, recovered, deaths, lastUpdate,}
   } catch (error) {
@@ -24,6 +31,17 @@ export const fetchDailyData = async () => {
 
     return modifiedData;
   } catch (error) {
+    console.log(error)
+  }
+}
 
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries }} = await axios.get(`${url}/countries`);
+    
+    return countries.map((country) => country.name)
+
+  } catch (error) {
+    console.log(error)
   }
 }
