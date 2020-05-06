7b2895b1a93f4b8acded1c1eafd29f0fe142297d
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-06 01:59:22 +0100

    feat: create CountryPicker to be able to select countries from the COVID-19 API

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { NativeSelect, FormControl } from '@material-ui/core'
 
-const CountryPicker = () => {
+import styles from './CountryPicker.module.css'
+
+import { fetchCountries } from '../../api'
+
+const CountryPicker = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries())
+    }
+
+    fetchAPI();
+  }, [setFetchedCountries]);
+
   return (
-    <div data-test="component-country-picker">CountryPicker</div>
+    <div data-test="component-country-picker">
+      <FormControl className={styles.FormControl}>
+        <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+          <option value="global">Global</option>
+        {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+        </NativeSelect>
+      </FormControl>
+    </div>
+    
+    // <div data-test="component-country-picker">CountryPicker</div>
   )
 }
 
