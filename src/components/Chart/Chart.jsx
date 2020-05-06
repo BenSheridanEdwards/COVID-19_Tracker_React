d85c48a55e6c07ec8202aaf434c30be2ef94bb0a
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-06 02:09:25 +0100

    feat(fix): display baar chart country data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -12,7 +12,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
       setDailyData(await fetchDailyData());
     }
     fetchAPI();
-  })
+  }, []);
 
   const lineChart = (
     dailyData.length
@@ -49,7 +49,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
               'rgba(0, 255, 0, 0.5)',
               'rgba(255, 0, 0, 0.5)',
           ],
-          data: [confirmed, recovered, deaths]
+          data: [confirmed.value, recovered.value, deaths.value]
           }]
           
         }}
