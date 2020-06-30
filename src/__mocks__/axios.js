c01fb236eeaf273b9c34b3389275d0855421dccc
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 20:49:49 +0100

    Chore: Fix formatting with eslint and prettier

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,5 +1,5 @@
 export default {
-  get: jest.fn(() => 
-    Promise.resolve({data: confirmed, recovered, deaths, lastUpdate })
+  get: jest.fn(() =>
+    Promise.resolve({ data: confirmed, recovered, deaths, lastUpdate })
   )
-}
+};
