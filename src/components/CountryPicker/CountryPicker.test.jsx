86dfd51067a1f8c063916410f3a5f18ec35d9c17
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 20:43:18 +0100

    Chore: Fix formatting with eslint and prettier

--- src/components/CountryPicker/CountryPicker.test.jsx
+++ src/components/CountryPicker/CountryPicker.test.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import { findByTestAttr } from '../../test/testUtils';
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../../test/testUtils";
 
-import CountryPicker from './CountryPicker';
+import CountryPicker from "./CountryPicker";
 
-const setup = (props={}) => {
-  const wrapper = shallow(<CountryPicker {...props} />)
+const setup = (props = {}) => {
+  const wrapper = shallow(<CountryPicker {...props} />);
   return wrapper;
-}
+};
 
-it('renders country picker component without error', () =>{
+it("renders country picker component without error", () => {
   const wrapper = setup();
-  const countryPickerComponent = findByTestAttr(wrapper, 'component-country-picker');
+  const countryPickerComponent = findByTestAttr(
+    wrapper,
+    "component-country-picker"
+  );
   expect(countryPickerComponent.length).toBe(1);
 });
