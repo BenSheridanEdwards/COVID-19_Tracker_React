1a24f77e37d3487710fb08bc2441d4efa6cb53b6
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 20:44:18 +0100

    Chore: Fix formatting with eslint and prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import { findByTestAttr } from './test/testUtils';
-import mockAxios from 'axios'
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "./test/testUtils";
+import mockAxios from "axios";
 
-import App from './App';
+import App from "./App";
 
-const setup = (props={}, state=null) => {
-  const wrapper = shallow(<App {...props} />)
-  if (state) wrapper.setState(state)
-    return wrapper;
-}
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
 
-it('renders app component without error', () =>{
+it("renders app component without error", () => {
   const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, 'component-app');
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
-it('calls axios and returns data from the COVID-19 api', async () => {
-  mockAxios.get.mockImplementationOnce(() => 
+it("calls axios and returns data from the COVID-19 api", async () => {
+  mockAxios.get.mockImplementationOnce(() =>
     Promise.resolve({
-      data: { 
-         confirmed, recovered, deaths, lastUpdate
+      data: {
+        confirmed,
+        recovered,
+        deaths,
+        lastUpdate
       }
-    }))
- 
-  const wrapper = setup()
-  wrapper.instance().componentDidMount()
+    })
+  );
+
+  const wrapper = setup();
+  wrapper.instance().componentDidMount();
   expect(mockAxios.get).toHaveBeenCalledTimes(3);
-  expect(mockAxios.get).toHaveBeenCalledWith("https://covid19.mathdro.id/api")
-  expect(wrapper.state()).toEqual({ country: "", data: {} })
-})
+  expect(mockAxios.get).toHaveBeenCalledWith("https://covid19.mathdro.id/api");
+  expect(wrapper.state()).toEqual({ country: "", data: {} });
+});
