32bc8e32a5305b3e9852010da3a2f9ea5ed39f4d
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 20:41:28 +0100

    Chore: Fix formatting with eslint and prettier

--- src/components/Chart/Chart.test.jsx
+++ src/components/Chart/Chart.test.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import { findByTestAttr } from '../../test/testUtils';
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../../test/testUtils";
 
-import Chart from './Chart.jsx';
+import Chart from "./Chart.jsx";
 
-const setup = (props={}) => {
-  const wrapper = shallow(<Chart {...props} />)
+const setup = (props = {}) => {
+  const wrapper = shallow(<Chart {...props} />);
   return wrapper;
-}
+};
 
-it('renders chart component without error', () =>{
-  const wrapper = setup( { data: { confirmed: 1000000, recovered: 900000, deaths: 100000} });
-  const chartComponent = findByTestAttr(wrapper, 'component-chart');
+it("renders chart component without error", () => {
+  const wrapper = setup({
+    data: { confirmed: 1000000, recovered: 900000, deaths: 100000 }
+  });
+  const chartComponent = findByTestAttr(wrapper, "component-chart");
   expect(chartComponent.length).toBe(1);
 });
