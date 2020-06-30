242ca64d83260bcad50549ce6ae7372d51c8ed77
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 20:38:41 +0100

    Chore: Fix formatting with eslint and prettier

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,66 +1,111 @@
-import React from 'react'
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
-import CountUp from 'react-countup';
-import cx from 'classnames';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import cx from "classnames";
 
-import styles from './Cards.module.css';
+import styles from "./Cards.module.css";
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
-  if(!confirmed) {
-    return 'Loading...'
+  if (!confirmed) {
+    return "Loading...";
   }
- 
+
   return (
     <div className={styles.container} data-test="component-cards">
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)} data-test="card-infected">
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+          data-test="card-infected"
+        >
           <CardContent>
-            <Typography color="testSecondary" gutterBottom> Infected </Typography>
+            <Typography color="testSecondary" gutterBottom>
+              {" "}
+              Infected{" "}
+            </Typography>
             <Typography variant="h5">
-              <CountUp 
-                start={0} 
-                end={confirmed.value} 
-                duration={2.5} 
-                separator="," 
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
               />
             </Typography>
-  <Typography color="testSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2"> Number of active cases of COVID-19 </Typography>
+            <Typography color="testSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              {" "}
+              Number of active cases of COVID-19{" "}
+            </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)} data-test="card-recovered">
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+          data-test="card-recovered"
+        >
           <CardContent>
-            <Typography color="testSecondary" gutterBottom> Recovered </Typography>
+            <Typography color="testSecondary" gutterBottom>
+              {" "}
+              Recovered{" "}
+            </Typography>
             <Typography variant="h5">
-              <CountUp 
-                start={0} 
-                end={recovered.value} 
-                duration={2.5} 
-                separator="," 
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=","
               />
             </Typography>
-            <Typography color="testSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2"> Number of recoveries from COVID-19 </Typography>
+            <Typography color="testSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              {" "}
+              Number of recoveries from COVID-19{" "}
+            </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)} data-test="card-deaths">
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+          data-test="card-deaths"
+        >
           <CardContent>
-            <Typography color="testSecondary" gutterBottom> Deaths </Typography>
+            <Typography color="testSecondary" gutterBottom>
+              {" "}
+              Deaths{" "}
+            </Typography>
             <Typography variant="h5">
-              <CountUp 
-                start={0} 
-                end={deaths.value} 
-                duration={2.5} 
-                separator="," 
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=","
               />
             </Typography>
-            <Typography color="testSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2"> Number of deaths caused by COVID-19 </Typography>
+            <Typography color="testSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              {" "}
+              Number of deaths caused by COVID-19{" "}
+            </Typography>
           </CardContent>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
